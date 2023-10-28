3cc10c2dee509ce75b48aae0702c22b1cb3ad411
Lucas <lucas_fasael@hotmail.com>
2023-10-28 00:23:47 -0300

    chuva

--- chuva/lib/views/profile_person_page.dart
+++ chuva/lib/views/profile_person_page.dart
@@ -13,6 +13,9 @@ class ProfilePersonPage extends StatefulWidget {
 class _ProfilePersonPageState extends State<ProfilePersonPage> {
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xff456189),
@@ -24,28 +27,28 @@ class _ProfilePersonPageState extends State<ProfilePersonPage> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 100,
-                    width: 100,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        "https://static.todamateria.com.br/upload/st/ep/stepheazul.jpg",
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://static.todamateria.com.br/upload/st/ep/stepheazul.jpg",
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Flexible(
-                  child: Column(
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -54,6 +57,7 @@ class _ProfilePersonPageState extends State<ProfilePersonPage> {
                         style: TextStyle(
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
+                          overflow: TextOverflow.ellipsis,
                         ),
                       ),
                       Text(
@@ -64,61 +68,62 @@ class _ProfilePersonPageState extends State<ProfilePersonPage> {
                         ),
                       ),
                     ],
-                  ),
-                )
-              ],
-            ),
-            const Flexible(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Bio",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "<p>Doutor em cosmologia, foi professor lucasiano emérito por Isaac Newton, Paul DIrac e Charles Babbage. Foi, pouco antes de falecer, diretor de pesquisa do Departamento de Matemática Aplicada e Física Teórica (CTC) da Universidade de Cambridge</p>",
-                  ),
+                  )
                 ],
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ActivityWidget()),
-                  );
-                });
-              },
-              child: const Text(
-                "Atividades",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+              SizedBox(
+                height: height * 0.25,
+                child: const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Bio",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "<p>Doutor em cosmologia, foi professor lucasiano emérito por Isaac Newton, Paul DIrac e Charles Babbage. Foi, pouco antes de falecer, diretor de pesquisa do Departamento de Matemática Aplicada e Física Teórica (CTC) da Universidade de Cambridge</p>",
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: Text(
-                "dom., 26/11/2023",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ActivityWidget()),
+                    );
+                  });
+                },
+                child: const Text(
+                  "Atividades",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            const Column(
-              children: [
-                ActivityWidget(),
-                ActivityWidget(),
-                ActivityWidget(),
-              ],
-            )
-          ],
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "dom., 26/11/2023",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Column(
+                children: [
+                  ActivityWidget(),
+                  ActivityWidget(),
+                  ActivityWidget(),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
